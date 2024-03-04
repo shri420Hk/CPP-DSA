5e6f4e126c055b31f5db900923522b5b4799cd2c
SRIKANT <76119503+shri420Hk@users.noreply.github.com>
2024-03-04 08:34:55 +0530

    Create Max_Min_in_Array.cpp

---
+++ Code Practice/Max_Min_in_Array.cpp
@@ -0,0 +1,45 @@
+#Finding the Maximumm and Minimu in an Array
+#include<iostream>
+#include <climits>
+using namespace std;
+int maxNum(int arr[], int size){
+    int max = INT_MIN;
+    for(int i = 0; i<size; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int minNum(int arr[], int size){
+    int min = INT_MAX;
+    for(int i = 0; i<size; i++){
+        if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+void printArr(int arr[], int size){
+    int i = 0;
+    cout<<"[";
+    while(i<5){
+        cout<<arr[i]<<" ";
+        i++;
+    }
+    cout<<"]\n";
+}
+int main(){
+    int arr[5];
+    cout<<"Enter the array Elements: \n";
+    for(int i = 0; i<5; i++){
+        cout<<"Enter the value of Index: "<< i <<"\n";
+        cin>>arr[i];
+    }
+    cout<<"Input array is: ";
+    printArr(arr, 5); 
+    int maximum = maxNum(arr, 5);
+    cout<<"Maximum value in the array is: "<<maximum<<"\n";
+    int minimum = minNum(arr, 5);
+    cout<<"Minimum value in the array is: "<<minimum<<"\n";
+}

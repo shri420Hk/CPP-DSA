9b453e376c9910856d9a1b3318bd0f6264c6dbc7
SRIKANT <76119503+shri420Hk@users.noreply.github.com>
2024-03-04 08:40:56 +0530

    Update Max_Min_in_Array.cpp

--- Code Practice/Max_Min_in_Array.cpp
+++ Code Practice/Max_Min_in_Array.cpp
@@ -3,19 +3,19 @@
 #include <climits>
 using namespace std;
 int maxNum(int arr[], int size){
-    int max = INT_MIN;
+    int max = INT_MIN;        //maximum = INT_MIN
     for(int i = 0; i<size; i++){
         if(arr[i]>max){
-            max = arr[i];
+            max = arr[i];    //we can use maximum = max(maximum , arr[i])
         }
     }
     return max;
 }
-int minNum(int arr[], int size){
-    int min = INT_MAX;
+int minNum(int arr[], int size) {
+    int min = INT_MAX;        //minimum = INT_MAX
     for(int i = 0; i<size; i++){
         if(arr[i]<min){
-            min = arr[i];
+            min = arr[i];   //we can use minimum = min(minimum, arr[i])
         }
     }
     return min;

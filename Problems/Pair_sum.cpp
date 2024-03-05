df7d2d34d57921ed3b3ae04eea87f9654f5cb32c
SRIKANT <76119503+shri420Hk@users.noreply.github.com>
2024-03-05 18:57:07 +0530

    Create Pair_sum.cpp

---
+++ Problems/Pair_sum.cpp
@@ -0,0 +1,22 @@
+//program prints the some of two consecutive numbers or pairs
+#include<iostream>
+using namespace std;
+void pairSum(int arr[], int size){
+    int i = 0;
+    int sum = 0;
+    cout<<"[";
+    while(i<size-1){
+        sum = arr[i]+arr[i+1];
+        cout<<arr[i]<<"+"<<arr[i+1]<<"="<<sum<<" ";
+
+        // i++;
+        i+=2;
+    }
+    cout<<arr[size-1];
+    cout<<"]";
+}
+int main(){
+    int arr[6]={1, 2, 3, 4, 5, 6};
+    cout<<"Sum of the pair elements present in the array: ";
+    pairSum(arr, 5);
+}

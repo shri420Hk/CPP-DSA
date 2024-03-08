17f713865d6b1cb194e5caff43291cd6debef950
SRIKANT <76119503+shri420Hk@users.noreply.github.com>
2024-03-08 07:15:08 +0530

    Create pair_sum_codestd.cpp

---
+++ pair_sum_codestd.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   // Write your code here.
+   vector<vector<int>> newvec;
+   int i = 0, j =0;
+   int val = arr.size();
+   for(i=0; i<val-1; i++){
+      for(j=i+1; j<val; j++){
+         if(arr[i]+arr[j]==s){
+            vector<int> temp;
+            temp.push_back(min(arr[i], arr[j]));
+            temp.push_back(max(arr[i], arr[j]));
+            newvec.push_back(temp);
+
+         }
+         
+      }
+   }
+   sort(newvec.begin(), newvec.end());
+   return newvec;
+}
